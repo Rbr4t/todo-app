75632e6f033116fb3d1b1806c5aef4247e205d8f
Robert <robertkoor0@gmail.com>
2022-08-19 19:24:13 +0300

    basic mockup UI done

---
+++ src/homepage.js
@@ -0,0 +1,10 @@
+function loadHome(){
+    const page = document.querySelector('#content');
+
+    // navbar
+    const navbar = document.createElement('div');
+    navbar.classList.add('navbar');
+
+    // logo and name
+    const logo = document.createElement('div')
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
+import './style.css';
+import Icon from './checkbox.png'
+import Sun from './sun.png'
+import Moon from './night-mode.png'
+
 console.log("HI!");
 console.log("HI!");
