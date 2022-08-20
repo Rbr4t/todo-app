accd4256e307c9d704cbf169684f1accfad81671
Robert <robertkoor0@gmail.com>
2022-08-20 20:45:41 +0300

    nodes can now be deleted

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import loadHome from './homepage.js'
 import populate from './populateTask.js'
 
 loadHome()
-// populate("test", 'test test test', '1,1,1')
 /*
     1. We need a factory module for the board
     2. We need a class for task card
@@ -15,22 +14,38 @@ const Board = (function(){
     const addBoard = (title, description, dueDate=null) =>{
         const newCard = new Card(title, description, dueDate)
         board.push(newCard)
+        console.log(newCard)
     }
-    return {board, addBoard}
+    const removeBoard = (index) => {
+        board.splice(index, 1)
+        removeTask[index].parentNode.remove()
+    };
+    return {board, addBoard, removeBoard}
 })()
+
 function Card(title, description, dueDate){
     this.title = title;
     this.description = description;
-    this.dueDate = dueDate
+    this.dueDate = dueDate;
 
     this.say = () => console.log(title);
 }
 
 
-
-Board.addBoard('test1', 'tets');
+Board.addBoard('Task1', 'Do smh');
 console.log(Board.board)
-Board.addBoard('test3', 'tets', "yeah");
+Board.addBoard('Task2', 'Don\'t do smh', "yeah");
 console.log(Board.board)
 
 Board.board.forEach(x => populate(x))
+
+// remove a task
+const removeTask = document.querySelectorAll('.remove');
+console.log(removeTask)
+removeTask.forEach((task, index) => {
+    task.addEventListener('click', e=> {
+        Board.removeBoard(index)
+        console.log(index)
+        
+    })
+})
--- src/populateTask.js
+++ src/populateTask.js
@@ -1,9 +1,13 @@
+let i = 0;
 export default function populate(obj){
     const todo = document.querySelector('.todo');
-
+    console.log(i)
     // card
     const div = document.createElement('div');
     div.classList.add('task');
+    div.setAttribute('id', i)
+    i += 1;
+    
     const h1 = document.createElement('h1');
     h1.textContent = obj.title;
     const p = document.createElement('p');
